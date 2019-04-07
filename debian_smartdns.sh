c24ad50896aa2185777d3868e775ab1819bc054f
Molly Lau <windowsbeta@outlook.com>
2019-04-07 14:27:39 +0800

    Update debian_smartdns.sh

--- debian_smartdns.sh
+++ debian_smartdns.sh
@@ -5,3 +5,8 @@ flag=$(ps aux | grep /usr/sbin/smartdns | grep -v "grep" | wc -l)
 if [ $flag = "0" ]; then
     /etc/init.d/smartdns start
 fi
+
+if ! grep -Eqi "127.0.0.1" /etc/resolv.conf; then
+    rm -rf /etc/resolv.conf
+    echo "nameserver 127.0.0.1" > /etc/resolv.conf
+fi
